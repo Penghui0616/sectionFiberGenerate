b4c9b7f3c1875f96ccd1d2442d5be0fcf2810963
Junjun Guo <guojj_ce@163.com>
2020-01-02 22:26:31 +0800

    updated

--- fiberGenerate.py
+++ fiberGenerate.py
@@ -33,6 +33,11 @@ class PolygonSection():
 		self.ax=ax
 		self.outNewNodeDict=None  #新生成的外侧保护层混凝土线节点字典
 		self.inNewNodeDict=None	  #新生成的内侧保护层混凝土线节点字典列表
+		self.lineWid=1 #线宽
+		self.coverColor="g" #保护层线颜色
+		self.coreColor="r" #核心混凝土颜色
+		self.barColor="k" #纵筋颜色
+		self.barMarkSize=15 #散点大小
 	def _lineNodeList(self,nodeDict,eleDict):
 		"""
 		返回每个线单元两端节点的X，Y坐标列表[([x1,x2],[y1,y2])]
@@ -57,22 +62,23 @@ class PolygonSection():
 		###外部轮廓线绘制
 		lineList=self._lineNodeList(self.outNode, self.outEle)
 		for each1 in lineList:
-			self.ax.plot(each1[0],each1[1], "r",zorder = 0)
+			self.ax.plot(each1[0],each1[1], self.coverColor,self.lineWid,zorder = 0)
 		if self.inNode != None:
 			for eachNode,eachEle in zip(self.inNode,self.inEle):
 				lineList1=self._lineNodeList(eachNode, eachEle)
 				for each2 in lineList1:
-					self.ax.plot(each2[0],each2[1], "r",zorder = 0)
+					self.ax.plot(each2[0],each2[1], self.coverColor,self.lineWid,zorder = 0)
 		else:
 			pass
 
-	def _middleLineNode(self,nodeDict,d0,pos="outLine"):
+	def _middleLineNode(self,nodeDict,d0,pos="outLine",zhole=1,yhole=1):
 		"""
 		计算分界线各节点的列表
 		输入：
 			nodeDict:节点字典 {1:(2.3,4.3)}
 			d0:保护层厚度
 			pos:外侧分界线（"outLinee"),内侧分界线("inLine")
+			zhole,yhole:局部z,y向内轮廓数量
 		返回：
 			NodeList:外分界线节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
 		"""
@@ -83,6 +89,8 @@ class PolygonSection():
 		for i1 in range(len(nodeDict)):
 			IterNode.append((NodeKeys[i1],NodeKeys[i1 + 1],NodeKeys[i1 + 2]))
 		NodeList = []
+		paraList1=[]
+		paraList2 = []
 		for each1 in IterNode:
 			nodeI = nodeDict[each1[0]]
 			nodeJ = nodeDict[each1[1]]
@@ -98,16 +106,57 @@ class PolygonSection():
 			c2 = (nodeKx - nodeJx) * nodeJy - (nodeKy - nodeJy) * nodeJx
 			c1_1 = c1 - math.sqrt(a1 ** 2 + b1 ** 2) * d0
 			c1_2 = c1 + math.sqrt(a1 ** 2 + b1 ** 2) * d0
+			c2_1 = c2 - math.sqrt(a2 ** 2 + b2 ** 2) * d0
+			c2_2 = c2 + math.sqrt(a2 ** 2 + b2 ** 2) * d0
+			paraList1.append((a1,b1,c1_1,c1_2))
+			paraList2.append((a2,b2,c2_1,c2_2))
+		indexChangeList=[]
+		if zhole==2:
+			horizonNum = []
+			horizonValue = []
+			for num,value1 in enumerate(paraList1):
+				if value1[0]==0:
+					horizonNum.append(num)
+					horizonValue.append(value1[2])
+			index1=horizonValue.index(min(np.abs(horizonValue)))
+			indexChange=horizonNum[index1]
+			indexChangeList.append((indexChange))
+			horizonNum2 = []
+			horizonValue2 = []
+			for num2, value2 in enumerate(paraList2):
+				if value2[0] == 0:
+					horizonNum2.append(num2)
+					horizonValue2.append(value2[2])
+			index2 = horizonValue2.index(min(np.abs(horizonValue2)))
+			indexChange2 = horizonNum2[index2]
+			indexChangeList.append((indexChange2))
+
+
+
+		for i2 in range(len(paraList1)):
+			c1_1=paraList1[i2][2]
+			c1_2 = paraList1[i2][3]
+			c2_1 = paraList2[i2][2]
+			c2_2 = paraList2[i2][3]
+			a1=paraList1[i2][0]
+			b1 = paraList1[i2][1]
+			a2 = paraList2[i2][0]
+			b2 = paraList2[i2][1]
 			if pos=="outLine":
 				c11 = c1_1 if abs(c1_1) < abs(c1_2) else c1_2
 			else:
-				c11 = c1_2 if abs(c1_1) < abs(c1_2) else c1_1
-			c2_1 = c2 - math.sqrt(a2 ** 2 + b2 ** 2) * d0
-			c2_2 = c2 + math.sqrt(a2 ** 2 + b2 ** 2) * d0
+				if i2!=indexChangeList[0]:
+					c11 = c1_2 if abs(c1_1) < abs(c1_2) else c1_1
+				else:
+					c11=c1_2 if abs(c1_1) > abs(c1_2) else c1_1
+
 			if pos=="outLine":
 				c22 = c2_1 if abs(c2_1) < abs(c2_2) else c2_2
 			else:
-				c22 = c2_2 if abs(c2_1) < abs(c2_2) else c2_1
+				if i2!=indexChangeList[1]:
+					c22 = c2_2 if abs(c2_1) < abs(c2_2) else c2_1
+				else:
+					c22 = c2_2 if abs(c2_1) > abs(c2_2) else c2_1
 			A = np.array([[a1, b1], [a2, b2]])
 			B = np.array([-c11, -c22])
 			newNode = list(solve(A, B))
@@ -124,13 +173,13 @@ class PolygonSection():
 		返回：
 			returnNodeList:外分界线节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
 		"""
-		returnNodeList=self._middleLineNode(self.outNode, d0, pos="outLine")
+		returnNodeList=self._middleLineNode(self.outNode, d0, pos="outLine",zhole=1,yhole=1)
 		outNodeDict={(i1+1):returnNodeList[i1] for i1 in range(len(returnNodeList))}
 		self.outNewNodeDict=outNodeDict  ##外层混凝土保护层线节点字典，与外轮廓节点一一对应
 		outEleDict=self.outEle
 		coverlineList = self._lineNodeList(outNodeDict, outEleDict)
 		for each2 in coverlineList:
-			self.ax.plot(each2[0], each2[1], "b",zorder = 0)
+			self.ax.plot(each2[0], each2[1], self.coverColor,self.lineWid,zorder = 0)
 		return returnNodeList
 
 	def innerLinePlot(self,d0):
@@ -142,20 +191,119 @@ class PolygonSection():
 			innerList:内分界线节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
 		"""
 		if self.inNode != None:
-			innerList=[]
-			innerListDict=[]
-			for eachNode,eachEle in zip(self.inNode,self.inEle):
-				returnNodeList = self._middleLineNode(eachNode, d0, pos="inLine")
-				innerList.append(returnNodeList)
-				innerListDict.append({(j1+1):returnNodeList[j1] for j1 in range(len(returnNodeList))})
-				inNodeDict = {(i1 + 1): returnNodeList[i1] for i1 in range(len(returnNodeList))}
-				inEleDict = eachEle
-				inlineList = self._lineNodeList(inNodeDict, inEleDict)
+			if len(self.inNode)==1:
+				pass
+			elif len(self.inNode)==2:
+				innerList=[]
+				innerListDict=[]
+				for eachNode,eachEle in zip(self.inNode,self.inEle):
+					returnNodeList = self._middleLineNode(eachNode, d0, pos="inLine",zhole=2,yhole=1)
+					innerList.append(returnNodeList)
+					innerListDict.append({(j1+1):returnNodeList[j1] for j1 in range(len(returnNodeList))})
+					inNodeDict = {(i1 + 1): returnNodeList[i1] for i1 in range(len(returnNodeList))}
+					inEleDict = eachEle
+					inlineList = self._lineNodeList(inNodeDict, inEleDict)
 
-				for each2 in inlineList:
-					self.ax.plot(each2[0], each2[1], "b",zorder = 0)
-			self.inNewNodeDict=innerListDict
-			return innerList
+					for each2 in inlineList:
+						self.ax.plot(each2[0], each2[1],self.coverColor,self.lineWid,zorder = 0)
+				self.inNewNodeDict=innerListDict
+				return innerList
+
+	def _lineSort(self,lineNodeList,xInc=0,yInc=0):
+		"""
+		封闭曲线以坐标轴对称的节点排序
+		输入：
+			lineNodeList:曲线节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
+			pos:外部轮廓线（outline),内部轮廓线（inLine)
+			xInc:按X坐标升序排列
+			yInc:按y坐标升序排列
+		返回：
+			outList1:增序
+			outList2:降序
+			outmin:最小值
+			outmax:最大值
+		"""
+		outNode1 = []
+		outNode2 = []
+		outList1=None
+		outList2=None
+		outmin=None
+		outmax=None
+		if yInc==1:
+			for num, node in enumerate(lineNodeList):
+				if node[0] > 0:
+					outNode1.append(node)
+				else:
+					outNode2.append(node)
+			outNode1Y = [each1[1] for each1 in outNode1]
+			index1Out = np.argsort(np.array(outNode1Y))
+			outList1 = [outNode1[each2] for each2 in index1Out]
+			outNode2Y = [each3[1] for each3 in outNode2]
+			index2Out = np.argsort(-np.array(outNode2Y))
+			outList2 = [outNode2[each4] for each4 in index2Out]
+			outmin = min(outNode1Y)
+			outmax = max(outNode1Y)
+		return outList1,outList2,outmin,outmax
+
+	def _twoHolePolygonDivide(self,outLineList,inLineList):
+		"""
+		具有两个内轮廓的多边形核心混凝土划分
+		输入：
+			outLineList:外分界线节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
+			inLineList:内分界线节点列表[[(x1,y1),(x2,y2),...,(xn,yn)],[(x1,y1),(x2,y2),...,(xn,yn)]]
+		返回:
+			list1,list2:多边形1及多边形2节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
+		"""
+		inLine1=inLineList[0]
+		inLine2=inLineList[1]
+
+		inLine1X=[each1[0] for each1 in inLine1]
+		inLine1Y =[each2[1] for each2 in inLine1]
+		if min(inLine1X)>0 or max(inLine1X)<0:
+			pass
+		elif min(inLine1Y)>0 or max(inLine1Y)<0:
+			inLineList0 = [each5[1] for each5 in inLineList[0]]
+			if min(inLineList0) > 0:
+				inList1 = inLineList[0]
+				inList2 = inLineList[1]
+			else:
+				inList1 = inLineList[1]
+				inList2 = inLineList[0]
+			outInc,outDec,outmin,outmax=self._lineSort(outLineList, xInc=0, yInc=1)
+			inUpperInc,inUpperDec,inUpperMin,inUpperMax=self._lineSort(inList1,xInc=0,yInc=1)
+			inUpperInc.reverse()
+			inUpperDec.reverse()
+			inLowerInc, inLowerDec, inLowerMin, inLowerMax = self._lineSort(inList2, xInc=0, yInc=1)
+			inLowerInc.reverse()
+			inLowerDec.reverse()
+			list1=[]
+			list2=[]
+			for each6 in outInc:
+				list1.append(each6)
+			list1.append((0,outmax))
+			list1.append(((0,inUpperMax)))
+			for each7 in inUpperInc:
+				list1.append(each7)
+			list1.append((0,inUpperMin))
+			list1.append((0,inLowerMax))
+			for each8 in inLowerInc:
+				list1.append(each8)
+			list1.append((0,inLowerMin))
+			list1.append((0,outmin))
+
+			for each9 in outDec:
+				list2.append(each9)
+			list2.append((0,outmin))
+			list2.append((0,inLowerMin))
+			for each10 in inLowerDec:
+				list2.append(each10)
+			list2.append((0,inLowerMax))
+			list2.append((0,inUpperMin))
+			for each11 in inUpperDec:
+				list2.append(each11)
+			list2.append((0,inUpperMax))
+			list2.append((0,outmax))
+		return list1,list2
 
 	def coreMesh(self,eleSize,outLineList,inLineList=None):
 		"""
@@ -163,10 +311,9 @@ class PolygonSection():
 		输入：
 			eleSize:纤维单元的边长
 			outLineList:外分界线节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
-			inLineList:内分界线节点列表[(x1,y1),(x2,y2),...,(xn,yn)]
+			inLineList:内分界线节点列表[[(x1,y1),(x2,y2),...,(xn,yn)],[(x1,y1),(x2,y2),...,(xn,yn)]]
 		返回：
 		"""
-
 		if inLineList==None:
 			geo=dmsh.Polygon(outLineList)
 		elif len(inLineList)==1:
@@ -174,15 +321,16 @@ class PolygonSection():
 				dmsh.Polygon(outLineList),
 				dmsh.Polygon(inLineList[0]),
 			)
-		elif len(inLineList)==2:
-			geo = dmsh.Difference(
-				dmsh.Polygon(outLineList),
-				dmsh.Polygon(inLineList[0]),
-				dmsh.Polygon(inLineList[1]),
-			)
 
-		points, elements = dmsh.generate(geo, eleSize)
-		self.ax.triplot(points[:, 0], points[:, 1], elements,zorder = 0)
+		elif len(inLineList)==2:
+			list1,list2=self._twoHolePolygonDivide(outLineList, inLineList)
+
+			geo1= dmsh.Polygon(list1)
+			geo2 = dmsh.Polygon(list2)
+			points1, elements1 = dmsh.generate(geo1, eleSize)
+			points2, elements2 = dmsh.generate(geo2, eleSize)
+			self.ax.triplot(points1[:, 0], points1[:, 1], elements1,c=self.coreColor,linewidth=self.lineWid,zorder = 0)
+			self.ax.triplot(points2[:, 0], points2[:, 1], elements2, c=self.coreColor,linewidth=self.lineWid,zorder=0)
 
 	def _coverDivide(self,outNodeDict,inNodeDict,eleDict,size,d0):
 		"""
@@ -274,11 +422,14 @@ class PolygonSection():
 			areaList.append(each1[2])
 		# self.ax.scatter(xList,yList,s=2,c="r")
 		for i1 in range(len(outNodeInfo)-1):
-			self.ax.plot([inNodeInfo[i1][0],outNodeInfo[i1][0]],[inNodeInfo[i1][1],outNodeInfo[i1][1]],"r",zorder = 0)
+			self.ax.plot([inNodeInfo[i1][0],outNodeInfo[i1][0]],[inNodeInfo[i1][1],outNodeInfo[i1][1]],self.coverColor,\
+						 linewidth=self.lineWid,zorder = 0)
 		for i2 in range(len(outNodeInfo)-1):
-			self.ax.plot([inNodeInfo[i2][0], inNodeInfo[i2+1][0]], [inNodeInfo[i2][1], inNodeInfo[i2+1][1]], "r",zorder = 0)
+			self.ax.plot([inNodeInfo[i2][0], inNodeInfo[i2+1][0]], [inNodeInfo[i2][1], inNodeInfo[i2+1][1]], \
+						 self.coverColor,linewidth=self.lineWid,zorder = 0)
 		for i3 in range(len(outNodeInfo)-1):
-			self.ax.plot([outNodeInfo[i3][0], outNodeInfo[i3+1][0]], [outNodeInfo[i3][1], outNodeInfo[i3+1][1]], "r",zorder = 0)
+			self.ax.plot([outNodeInfo[i3][0], outNodeInfo[i3+1][0]], [outNodeInfo[i3][1], outNodeInfo[i3+1][1]],\
+						 self.coverColor,linewidth=self.lineWid,zorder = 0)
 		##内部保护层混凝土的划分
 		if self.inNode != None:
 			nInhole=len(self.inNode)
@@ -294,11 +445,14 @@ class PolygonSection():
 					inyList.append(each2[1])
 					inareaList.append(each2[2])
 				for i5 in range(len(innerOut)-1):
-					self.ax.plot([innerOut[i5][0], innerIn[i5][0]], [innerOut[i5][1], innerIn[i5][1]], "r",zorder = 0)
+					self.ax.plot([innerOut[i5][0], innerIn[i5][0]], [innerOut[i5][1], innerIn[i5][1]], \
+								 self.coverColor,linewidth=self.lineWid,zorder = 0)
 				for i6 in range(len(innerOut)-1):
-					self.ax.plot([innerIn[i6][0], innerIn[i6 + 1][0]], [innerIn[i6][1], innerIn[i6 + 1][1]],"r",zorder = 0)
+					self.ax.plot([innerIn[i6][0], innerIn[i6 + 1][0]], [innerIn[i6][1], innerIn[i6 + 1][1]],\
+								 self.coverColor,linewidth=self.lineWid,zorder = 0)
 				for i7 in range(len(innerIn)-1):
-					self.ax.plot([innerOut[i7][0], innerOut[i7 + 1][0]],[innerOut[i7][1],innerOut[i7 + 1][1]], "r",zorder = 0)
+					self.ax.plot([innerOut[i7][0], innerOut[i7 + 1][0]],[innerOut[i7][1],innerOut[i7 + 1][1]],\
+								 self.coverColor,linewidth=self.lineWid,zorder = 0)
 			# self.ax.scatter(inxList,inyList,s=2,c="r")
 
 	def _barDivide(self,barD,barDist,nodeDict,lineEleDict):
@@ -354,14 +508,14 @@ class PolygonSection():
 		outBarListEle=self.outEle
 		#外侧钢筋的划分
 		outBarFiber,outXList,outYList=self._barDivide(outBarD,outBarDist,outBarLineDict,outBarListEle)
-		self.ax.scatter(outXList,outYList,s=10,c="k",zorder = 2)
+		self.ax.scatter(outXList,outYList,s=self.barMarkSize,c=self.barColor,linewidth=self.lineWid,zorder = 2)
 		if self.inNode != None:
 			inBarLineDict = self.inNewNodeDict  # 新生成的内侧保护层混凝土线节点字典列表
 			inBarLineEle = self.inEle
 			nEle=len(inBarLineEle)
 			for i1 in range(nEle):
 				inBarFiber, inXList, inYList = self._barDivide(inBarD, inBarDist, inBarLineDict[i1], inBarLineEle[i1])
-				self.ax.scatter(inXList, inYList, s=10, c="k", zorder=2)
+				self.ax.scatter(inXList, inYList, s=self.barMarkSize, c=self.barColor,linewidth=self.lineWid, zorder=2)
 
 
 class CircleSection():
@@ -504,7 +658,7 @@ class CircleSection():
 		self.ax.scatter(outFiberXList,outFiberYList,s=10,c="k",zorder = 3)
 		if self.inD!=None:
 			inFiberInfo, inFiberXList, inFiberYList = self._barDivide(inBarD, inBarDist, pos="in")
-			self.ax.scatter(inFiberXList, inFiberYList, s=10, c="k", zorder=3)
+			self.ax.scatter(inFiberXList, inFiberYList, s=15, c="k", zorder=3)
 
 
 if __name__=="__main__":
@@ -520,36 +674,45 @@ if __name__=="__main__":
 	# inSideEle=[{1:(1,2),2:(2,3),3:(3,4),4:(4,5),5:(5,6),6:(6,7),7:(7,8),8:(8,9),9:(9,10),10:(10,11),11:(11,12),12:(12,1)}]
 	######################################################################################
 	##########################---中塔柱截面---#############################################
-	outSideNode={1:(3.5,3),2:(1.5,5),3:(-1.5,5),4:(-3.5,3),5:(-3.5,-3),6:(-1.5,-5),7:(1.5,-5),8:(3.5,-3)}
-	outSideEle = {1: (1, 2), 2: (2, 3), 3: (3, 4), 4: (4, 5), 5: (5, 6), 6: (6, 7), 7: (7, 8), 8: (8, 1)}
-	inSideNode=[{1:(1.9,2.4),2:(1.1,3.2),3:(-1.1,3.2),4:(-1.9,2.4),5:(-1.9,-2.4),6:(-1.1,-3.2),7:(1.1,-3.2),8:(1.9,-2.4)}]
-	inSideEle = [{1: (1, 2), 2: (2, 3), 3: (3, 4), 4: (4, 5), 5: (5, 6), 6: (6, 7), 7: (7, 8), 8: (8, 1)}]
+	# outSideNode={1:(3.5,3),2:(1.5,5),3:(-1.5,5),4:(-3.5,3),5:(-3.5,-3),6:(-1.5,-5),7:(1.5,-5),8:(3.5,-3)}
+	# outSideEle = {1: (1, 2), 2: (2, 3), 3: (3, 4), 4: (4, 5), 5: (5, 6), 6: (6, 7), 7: (7, 8), 8: (8, 1)}
+	# inSideNode=[{1:(1.9,2.4),2:(1.1,3.2),3:(-1.1,3.2),4:(-1.9,2.4),5:(-1.9,-2.4),6:(-1.1,-3.2),7:(1.1,-3.2),8:(1.9,-2.4)}]
+	# inSideEle = [{1: (1, 2), 2: (2, 3), 3: (3, 4), 4: (4, 5), 5: (5, 6), 6: (6, 7), 7: (7, 8), 8: (8, 1)}]
 	######################################################################################
 	##########################---下塔柱截面---#############################################
 
 	#########---多边形截面内有两个洞--#####################################################
-
-
+	outSideNode = {1: (4.5,6.655), 2: (2.5, 8.655), 3: (-2.5,8.655), 4: (-4.5, 6.655), 5: (-4.5, -6.655), 6: (-2.5, -8.655), 7: (2.5, -8.655),
+				   8: (4.5, -6.655)}
+	outSideEle = {1: (1, 2), 2: (2, 3), 3: (3, 4), 4: (4, 5), 5: (5, 6), 6: (6, 7), 7: (7, 8), 8: (8, 1)}
+	inSideNode = [{1: (2.5, 5.855), 2: (1.7, 6.655), 3: (-1.7,6.655), 4: (-2.5, 5.855), 5: (-2.5, 1.3), 6: (-1.7, 0.5), 7: (1.7, 0.5),8: (2.5,1.3)},
+				  {1:(2.5,-1.3),2:(1.7,-0.5),3:(-1.7,-0.5),4:(-2.5,-1.3),5:(-2.5,-5.855),6:(-1.7,-6.655),7:(1.7,-6.655),8:(2.5,-5.855)}]
+	inSideEle = [{1: (1, 2), 2: (2, 3), 3: (3, 4), 4: (4, 5), 5: (5, 6), 6: (6, 7), 7: (7, 8), 8: (8, 1)},{1: (1, 2), 2: (2, 3), 3: (3, 4), 4: (4, 5), 5: (5, 6), 6: (6, 7), 7: (7, 8), 8: (8, 1)}]
 	######################################################################################
 	fig = plt.figure(figsize=(3.5,5))
 	ax = fig.add_subplot(111)
 	d0=0.2  #保护层厚度
-	eleSize=0.3  #核心纤维的大小
+	eleSize=0.4 #核心纤维的大小
 	coverSize=0.3 #保护层纤维大小
-	outBarDist=0.3
+	outBarDist=0.4
 	outBarD=0.032
-	inBarDist=0.3
+	inBarDist=0.4
 	inBarD=0.032
-	sectInstance=PolygonSection(ax,outSideNode,outSideEle)
+	sectInstance=PolygonSection(ax,outSideNode,outSideEle,inSideNode,inSideEle)
 	sectInstance.sectPlot()
 	outLineList=sectInstance.coverLinePlot(d0)
-	# inLineList=sectInstance.innerLinePlot(d0)
-	sectInstance.coreMesh(eleSize,outLineList)
-	# print(len(sectInstance.inNewNodeDict))
-	# print(sectInstance.outNewNodeDict)
+	inLineList=sectInstance.innerLinePlot(d0)
+	sectInstance.coreMesh(eleSize,outLineList,inLineList)
 	sectInstance.coverMesh(coverSize,d0)
-	sectInstance.barMesh(outBarD,outBarDist)
+	sectInstance.barMesh(outBarD,outBarDist,inBarD,inBarDist)
 	plt.show()
+
+
+
+
+
+
+
 
 	######################################################################################
 	#########################################---圆形截面---################################
@@ -574,6 +737,5 @@ if __name__=="__main__":
 	#
 	# plt.show()
 
-	print(help(PolygonSection))
 
 
